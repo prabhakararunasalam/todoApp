95b6c5566ba8860a296b93830c4e6c4a8e708960
prabhakararunasalam <prabhakararunasalam17@gmail.com>
2024-11-13 11:26:19 +0530

    completed

--- script.js
+++ script.js
@@ -99,5 +99,5 @@ const deleteTask = (e) => {
   e.parentElement.parentElement.remove();
   data.splice(e.parentElement.parentElement.id, 1);
   localStorage.setItem("data", JSON.stringify(data));
-  console.log(data);
+//   console.log(data);
 };
